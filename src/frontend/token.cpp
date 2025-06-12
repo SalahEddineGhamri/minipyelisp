1a09babf57e2096f5c7a87056ac240dabf040408
Salah <salaheddine.ghamri@yahoo.com>
2025-06-12 23:56:52 +0200

    lexer structure

---
+++ src/frontend/token.cpp
@@ -0,0 +1,26 @@
+
+#include "token.hpp"
+#include "frontend/lexer.hpp"
+#include <map>
+
+namespace minipyelisp::lexer {
+
+// helper: map token types to string
+static const std::map<TokenType, std::string> tokenTypeStrings = {};
+
+std::string Token::to_string() const {
+
+  std::string str_token = "UNKNOWN";
+
+  if (type == TokenType::END_OF_FILE) {
+    str_token = "EOF token";
+  }
+
+  if (type == TokenType::INT_LITERAL) {
+    str_token = "INT token " + value;
+  }
+
+  return str_token;
+};
+
+} // namespace minipyelisp::lexer
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,32 @@
 /*
-  main of minipyelisp
-*/
+ * minipyelisp
+ */
 
+#include "frontend/lexer.hpp"
+#include "token.hpp"
 #include <iostream>
 
 int main() {
-  std::cout << "Hello from minipyelisp!" << std::endl;
+
+  /*
+  std::string source = "x = 10 + 20\n"
+                       "if x == 30:\n"
+                       "    print(\"Hello\")\n"
+                       "else:\n"
+                       "    y = 'world'\n"
+                       "    # This is a comment\n"
+                       "def foo(a, b):\n"
+                       "    return a + b\n";
+  */
+
+  std::string source = " 20 \n";
+
+  minipyelisp::lexer::Lexer lexer(source);
+
+  std::vector<minipyelisp::lexer::Token> tokens = lexer.tokenize();
+
+  // to debug
+  for (const auto &token : tokens) {
+    std::cout << token.to_string() << std::endl;
+  }
 }
